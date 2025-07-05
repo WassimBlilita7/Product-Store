04c946f6911bf28c518591d6847cb88277ae0e32
wassimblilita7 <wassim.blilita19@gmail.com>
2025-07-05 19:12:13 +0200

    Ready for deploymenet

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,13 @@ import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
 import { aj } from "./lib/arcjet.js";
+import path from "path"
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
@@ -52,6 +54,13 @@ app.use(async(req,res,next)=>{
 
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 
 async function initDB() {
     try {
